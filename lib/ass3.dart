e1c5b3b35207aa11468c6c93d0fa01d0fae51997
zihadsikder <zihadsikder.cse@gmail.com>
2023-09-16 20:08:24 +0600

    center the table

--- lib/ass3.dart
+++ lib/ass3.dart
@@ -1,27 +1,17 @@
 import 'dart:io';
 
 void main() {
-  // Read input values
-  stdout.write("Enter l1, r1, l2, and r2 separated by spaces: ");
-  List<int> input = stdin.readLineSync()!.split(' ').map((e) => int.parse(e)).toList();
+  // Read the screen width (W)
+  stdout.write("Enter the screen width (W): ");
+  int screenWidth = int.parse(stdin.readLineSync()!);
 
-  // Extract values
-  int l1 = input[0];
-  int r1 = input[1];
-  int l2 = input[2];
-  int r2 = input[3];
+  // Fixed table width (TW)
+  int tableWidth = 300;
 
-  // Calculate the gap
-  int gapStart = r2 + 1;
-  int gapEnd = l2 - 1;
+  // Calculate the horizontal position of the table
+  int tablePosition = (screenWidth - tableWidth) ~/ 2;
 
-  // Print the two segments
-  for (int i = l1; i <= gapEnd; i++) {
-    stdout.write("$i ");
-  }
-  for (int i = gapStart; i <= r1; i++) {
-    stdout.write("$i ");
-  }
-
-  print(""); // Print a newline to separate the segments
+  // Print the table's horizontal position
+  print("Horizontal position of the table: $tablePosition pixels");
 }
+
