061a9bb2a381ce410aa1bc5f1173bb93db024478
zihadsikder <zihadsikder.cse@gmail.com>
2023-11-03 22:11:07 +0600

    Write a program to create a function that takes two arguments: the original price and the discount percentage as integers and returns the final price after the discount.

--- lib/ass3.dart
+++ lib/ass3.dart
@@ -1,50 +1,31 @@
-//import 'dart:io';
+import 'dart:io';
+void main(){
+  List<String> input = stdin.readLineSync()!.split(" ");
+
+  double discount = (int.parse(input[0]) / 100) * int.parse(input[1]);
+print("Price: ${(int.parse(input[0]) - discount).toStringAsFixed(2)}");
+
+}
+// import 'dart:io';
+//
+// double calculateFinalPrice(int originalPrice, int discountPercentage) {
+//
+//   double discount = originalPrice * (discountPercentage / 100);
+//   double finalPrice = originalPrice - discount;
+//   return finalPrice;
+// }
 //
 // void main() {
-//   // Step 1: Parse the input
-//   List<int> values = stdin.readLineSync()!.split(' ').map(int.parse).toList();
-//   String sequence = stdin.readLineSync()!;
-//   int temp = values[0] ;
-//   // Step 2: Swap the values according to the given sequence
-//   for (int i = 0; i < sequence.length; i++) {
+//   // Read the original price and discount percentage from the user
+//   print("Enter the original price: ");
+//   int originalPrice = int.parse(stdin.readLineSync()!);
 //
-//     if (sequence[i] == 'A') {
-//       values[0] = values[2];
-//     } else if (sequence[i] == 'B') {
-//       values[2] = values[1];
-//     } else if (sequence[i] == 'C') {
-//       values[1] = temp;
-//     }
-//   }
+//   print("Enter the discount percentage: ");
+//   int discountPercentage = int.parse(stdin.readLineSync()!);
 //
-//   // Step 3: Print the final values in the specified order
-//   print('${values[0]} ${values[1]} ${values[2]}');
+//   // Calculate the final price
+//   double finalPrice = calculateFinalPrice(originalPrice, discountPercentage);
+//
+//   // Print the final price
+//   print("${finalPrice.toStringAsFixed(2)}");
 // }
-import 'dart:io';
-
-void main() {
-  var input = stdin.readLineSync()!.split(' ');
-  var a = int.parse(input[0]);
-  var b = int.parse(input[1]);
-  var c = int.parse(input[2]);
-  var order = stdin.readLineSync()!.split('');
-  Map newOrder = {
-    order[0]: a,
-    order[1]: b,
-    order[2]: c,
-  };
-  newOrder.forEach(
-        (key, value) {
-      if (key == "A") {
-        
-        newOrder["A"] = c;
-      } else if (key == "B") {
-        newOrder["B"] = a;
-      } else {
-        newOrder["C"] = b;
-      }
-      ;
-    },
-  );
-  print("${newOrder[order[0]]} ${newOrder[order[1]]} ${newOrder[order[2]]}");
-}
