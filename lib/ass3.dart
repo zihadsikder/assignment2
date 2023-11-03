6efe34f5144f626304722b5423c7c64bc7e23565
zihadsikder <zihadsikder.cse@gmail.com>
2023-11-03 22:55:50 +0600

    Suppose, you work in a toy car workshop, and your job is to build toy cars from a collection of parts. Each toy car needs 4 wheels, 1 car body, and 2 figures of people to be placed inside. Write a program which will calculate how many complete toy cars can be made, given the total number of wheels, car bodies and figures available.

--- lib/ass3.dart
+++ lib/ass3.dart
@@ -1,28 +1,57 @@
+// import 'dart:io';
+// import 'dart:math';
+// void main() {
+//   //print("Enter numbers:");
+//
+//   String input = stdin.readLineSync()!;
+//   List<String> parts = input.split(' ');
+//
+//   if (parts.length != 3) {
+//     print("Invalid input. Please enter three integers separated by spaces.");
+//     return;
+//   }
+//
+//   int wheels = int.tryParse(parts[0]) ?? 0;
+//   int carBodies = int.tryParse(parts[1]) ?? 0;
+//   int figures = int.tryParse(parts[2]) ?? 0;
+//
+//   if (wheels < 4 || carBodies < 1 || figures < 2) {
+//     print("Insufficient parts to build any cars.");
+//     return;
+//   }
+//
+//   int maxCars = 0;
+//
+//   maxCars = wheels ~/ 4; // Maximum number of cars based on wheels
+//   maxCars = min(maxCars, carBodies); // Maximum number of cars based on car bodies
+//   maxCars = min(maxCars, figures ~/ 2); // Maximum number of cars based on figures
+//
+//   print(maxCars);
+// }
 import 'dart:io';
-
-bool isFunny(String s) {
-  for (int i = 0; i < s.length; i += 2) {
-    if (s[i] != s[i].toLowerCase()) {
-      return false;
-    }
-  }
-
-  for (int i = 1; i < s.length; i += 2) {
-    if (s[i] != s[i].toUpperCase()) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
+import 'dart:math';
 void main() {
+  print("Enter numbers:");
 
-  String s = stdin.readLineSync()!;
+  String input = stdin.readLineSync()!;
+  List<String> parts = input.split(' ');
 
-  if (isFunny(s)) {
-    print("Yes");
+  if (parts.length != 3) {
+    print("Invalid input. Please enter three integers separated by spaces.");
+    return;
+  }
+
+  int wheels = int.tryParse(parts[0]) ?? 0;
+  int carBodies = int.tryParse(parts[1]) ?? 0;
+  int figures = int.tryParse(parts[2]) ?? 0;
+
+  if (wheels < 4 || carBodies < 1 || figures < 2) {
+    print("0");
   } else {
-    print("No");
+    int maxCars = 0;
+    maxCars = wheels ~/ 4;
+    maxCars = min(maxCars, carBodies);
+    maxCars = min(maxCars, figures ~/ 2);
+    print(maxCars);
   }
 }
