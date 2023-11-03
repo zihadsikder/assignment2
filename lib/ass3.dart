7802f7aabac94cd0954a14f8dd6460b4867e0185
zihadsikder <zihadsikder.cse@gmail.com>
2023-11-03 22:46:34 +0600

    We call a string funny if its odd positioned ( 1 � � , 3 � � , 5 � ℎ , . . . 1st,3rd,5th,... from the beginning) characters are all lowercase English letters and its even positioned characters ( 2 � � , 4 � ℎ , 6 � ℎ , . . . 2nd,4th,6th,... from the beginning) are all uppercase English letters. You are given a string � S. You have to find out if � S is funny or not.

--- lib/ass3.dart
+++ lib/ass3.dart
@@ -1,108 +1,28 @@
-// import 'dart:io';
-//
-// int countTrueValues(List<String> boolArray) {
-//   int count = 1;
-//
-//   // Iterate through the array and count the true values
-//   for (String value in boolArray) {
-//     if (value == true) {
-//       count++;
-//     }
-//   }
-//
-//   return count;
-// }
-//
-// void main() {
-//
-//
-//     //int length = int.parse(stdin.readLineSync()!);
-//
-//   List<String> boolArray = stdin.readLineSync()!.split(' ');
-//
-//   if (boolArray.length-2 != boolArray[0]) {
-//     print("Invalid input length. Please provide ${boolArray[0]} boolean values.");
-//     return;
-//   }
-//
-//   int trueCount = countTrueValues(boolArray);
-//
-//   print("$trueCount");
-// }
-// import 'dart:io';
-//
-// int countTrueValues(List<bool> boolArray) {
-//   // Initialize a counter to count the true values
-//   int count = 0;
-//
-//   // Iterate through the array and count the true values
-//   for (bool value in boolArray) {
-//     if (value == true) {
-//       count++;
-//     }
-//   }
-//
-//   return count;
-// }
-//
-// void main() {
-//   // Read the length of the boolean array from the user
-//
-//   int length = int.parse(stdin.readLineSync()!);
-//
-//   // Read the boolean values as space-separated integers and convert them to bool
-//
-//   List<bool> boolArray = stdin.readLineSync()!.split(' ').map((e) => e == '1').toList();
-//
-//   if (boolArray.length != length) {
-//     print("Invalid input length. Please provide $length boolean values.");
-//     return;
-//   }
-//
-//   // Calculate the count of true values
-//   int trueCount = countTrueValues(boolArray);
-//
-//   // Print the count of true values
-//   print("$trueCount");
-// }
 import 'dart:io';
 
-int countTrueValues(List<bool> boolArray) {
-  int count = 0;
-
-  for (bool value in boolArray) {
-    if (value == true) {
-      count++;
+bool isFunny(String s) {
+  for (int i = 0; i < s.length; i += 2) {
+    if (s[i] != s[i].toLowerCase()) {
+      return false;
     }
   }
 
-  return count;
+  for (int i = 1; i < s.length; i += 2) {
+    if (s[i] != s[i].toUpperCase()) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 void main() {
-  print("Enter the length of the array and the boolean values separated by a space:");
-  String input = stdin.readLineSync()!;
-  List<String> parts = input.split(' ');
 
-  if (parts.length < 2) {
-    print("Invalid input. Please provide the length of the array and boolean values.");
-    return;
+  String s = stdin.readLineSync()!;
+
+  if (isFunny(s)) {
+    print("Yes");
+  } else {
+    print("No");
   }
-
-  int? length = int.tryParse(parts[0]);
-  if (length == null) {
-    print("Invalid array length. Please enter a valid integer.");
-    return;
-  }
-
-  List<bool> boolArray = parts.sublist(1).map((element) => element == '1').toList();
-
-  if (boolArray.length != length) {
-    print("Invalid input length. Please provide $length boolean values.");
-    return;
-  }
-
-  int trueCount = countTrueValues(boolArray);
-
-  print("$trueCount");
 }
