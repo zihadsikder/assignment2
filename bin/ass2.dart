0bec7b5272a4af9a7d9e632e5786c411596df6c5
zihadsikder <zihadsikder.cse@gmail.com>
2023-11-03 22:56:15 +0600

    update

--- bin/ass2.dart
+++ bin/ass2.dart
@@ -842,3 +842,35 @@
 //
 //   return digits.join();
 // }
+///Write a program to create a function that takes two arguments: the original price and the discount percentage as integers and returns the final price after the discount.
+// import 'dart:io';
+// void main(){
+//   List<String> input = stdin.readLineSync()!.split(" ");
+//
+//   double discount = (int.parse(input[0]) / 100) * int.parse(input[1]);
+// print("Price: ${(int.parse(input[0]) - discount).toStringAsFixed(2)}");
+//
+// }
+// import 'dart:io';
+//
+// double calculateFinalPrice(int originalPrice, int discountPercentage) {
+//
+//   double discount = originalPrice * (discountPercentage / 100);
+//   double finalPrice = originalPrice - discount;
+//   return finalPrice;
+// }
+//
+// void main() {
+//   // Read the original price and discount percentage from the user
+//   print("Enter the original price: ");
+//   int originalPrice = int.parse(stdin.readLineSync()!);
+//
+//   print("Enter the discount percentage: ");
+//   int discountPercentage = int.parse(stdin.readLineSync()!);
+//
+//   // Calculate the final price
+//   double finalPrice = calculateFinalPrice(originalPrice, discountPercentage);
+//
+//   // Print the final price
+//   print("${finalPrice.toStringAsFixed(2)}");
+// }
