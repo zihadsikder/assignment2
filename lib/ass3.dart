21a68e7ee7eb129f9ad4c685f14c0b020936ab5c
zihadsikder <zihadsikder.cse@gmail.com>
2023-11-09 00:11:24 +0600

    update

--- lib/ass3.dart
+++ lib/ass3.dart
@@ -1,33 +1,4 @@
-// import 'dart:io';
-// import 'dart:math';
-// void main() {
-//   //print("Enter numbers:");
-//
-//   String input = stdin.readLineSync()!;
-//   List<String> parts = input.split(' ');
-//
-//   if (parts.length != 3) {
-//     print("Invalid input. Please enter three integers separated by spaces.");
-//     return;
-//   }
-//
-//   int wheels = int.tryParse(parts[0]) ?? 0;
-//   int carBodies = int.tryParse(parts[1]) ?? 0;
-//   int figures = int.tryParse(parts[2]) ?? 0;
-//
-//   if (wheels < 4 || carBodies < 1 || figures < 2) {
-//     print("Insufficient parts to build any cars.");
-//     return;
-//   }
-//
-//   int maxCars = 0;
-//
-//   maxCars = wheels ~/ 4; // Maximum number of cars based on wheels
-//   maxCars = min(maxCars, carBodies); // Maximum number of cars based on car bodies
-//   maxCars = min(maxCars, figures ~/ 2); // Maximum number of cars based on figures
-//
-//   print(maxCars);
-// }
+
 import 'dart:io';
 import 'dart:math';
 void main() {
