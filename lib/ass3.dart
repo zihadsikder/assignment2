c46809c1a5cba00c3f50c6da9206e2d005901c30
zihadsikder <zihadsikder.cse@gmail.com>
2023-11-16 00:39:48 +0600

    For being lazy you are punished to walk on the garden at least N steps. As you mastered laziness, after ℎ ith step you take rest for i second(s). You also takes X second(s) to complete each step. Now write a program to calculate the total time to take exactly N steps.

--- lib/ass3.dart
+++ lib/ass3.dart
@@ -1,14 +1,15 @@
 import 'dart:io';
 
 void main() {
-  List<int> inputs = stdin.readLineSync()!.split(' ').map(int.parse).toList();
-  int N = inputs[0];
-  int A = inputs[1];
-  int B = inputs[2];
 
-  if (N >= (A + B)) {
-    print("Yes");
-  } else {
-    print("No");
+  List<int> input = stdin.readLineSync()!.split(' ').map(int.parse).toList();
+  int N = input[0];
+  int X = input[1];
+
+  int resttime = 0;
+  for (int i = 1; i < N; i++) {
+    resttime += i;
   }
+
+  print("${resttime+ N*X}");
 }
